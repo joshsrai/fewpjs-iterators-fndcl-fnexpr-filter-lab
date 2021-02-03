fbb1fe6f7197db45d2117fbc8b01c5b343e12073
joshsrai <joshsrai@gmail.com>
2021-02-03 17:04:04 -0500

    commit tree 23f571d5730006ebeee5f93993799beffb42d2a5

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+function findMatching(drivers, string){
+    return drivers.filter(driver => {
+        return driver.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(drivers => {
+        return drivers.charAt(0) === string.charAt(0)
+    })
+}
+
+function matchName(drivers, string) {
+    return drivers.filter(drivers => {
+        return drivers.name === string
+    })
+}
